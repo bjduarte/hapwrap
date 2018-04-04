7a75f5d39ff9103cec9b61371c67c45189f7e1fe
cpr2829 <pchaudh3@asu.edu>
2018-04-03 18:46:31 -0700

    Code sample demo-1 Modified example - /python/examples/strandtest.py

---
+++ python/examples/strandtest.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+
+import time
+from neopixel import *
+import argparse
+
+# LED strip configuration:
+LED_COUNT      = 24      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+#Define functions which detect object,elevation,distance:
+def buzz(strip,combo):
+    
+    
+    pulse_color = Color(255, 0, 0)
+    direction_color = Color(0, 255, 0)
+    iter =0 
+    if combo[0]==0:
+        min_motor=0
+    elif combo[0]==1:
+        min_motor=8
+    else:
+        min_motor=16
+    main_motor = min_motor+combo[2]
+    strip.setPixelColor(main_motor,direction_color)
+    strip.show()
+    time.sleep(wait_ms/1000.0)
+    while iter<3:
+        for i in range(min_motor,min_motor+8)):
+        strip.setPixelColor(i, pulse_color)
+        strip.show()
+        if combo[1]==10:
+            time.sleep(wait_ms/1000.0)
+        if combo[1]==15:
+            time.sleep(wait_ms/500.0)
+        if combo[1]==20:
+            time.sleep(wait_ms)
+        if combo[1]==10:
+            time.sleep(wait_ms*2)
+        iter +=1
+
+
+
+# Define functions which animate LEDs in various ways.
+# def colorWipe(strip, color, wait_ms=50):
+#     """Wipe color across display a pixel at a time."""
+#     for i in range(strip.numPixels()):
+#         strip.setPixelColor(i, color)
+#         strip.show()
+#         time.sleep(wait_ms/1000.0)
+
+# def theaterChase(strip, color, wait_ms=50, iterations=10):
+#             for i in range(0, strip.numPixels(), 3):
+#                 strip.setPixelColor(i+q, color)
+#             strip.show()
+#             time.sleep(wait_ms/1000.0)
+#             for i in range(0, strip.numPixels(), 3):
+#     """Movie theater light style chaser animation."""
+#     for j in range(iterations):
+#         for q in range(3):
+#                 strip.setPixelColor(i+q, 0)
+
+# def wheel(pos):
+#     """Generate rainbow colors across 0-255 positions."""
+#     if pos < 85:
+#         return Color(pos * 3, 255 - pos * 3, 0)
+#     elif pos < 170:
+#         pos -= 85
+#         return Color(255 - pos * 3, 0, pos * 3)
+#     else:
+#         pos -= 170
+#         return Color(0, pos * 3, 255 - pos * 3)
+
+# def rainbow(strip, wait_ms=20, iterations=1):
+#     """Draw rainbow that fades across all pixels at once."""
+#     for j in range(256*iterations):
+#         for i in range(strip.numPixels()):
+#             strip.setPixelColor(i, wheel((i+j) & 255))
+#         strip.show()
+#         time.sleep(wait_ms/1000.0)
+
+# def rainbowCycle(strip, wait_ms=20, iterations=5):
+#     """Draw rainbow that uniformly distributes itself across all pixels."""
+#     for j in range(256*iterations):
+#         for i in range(strip.numPixels()):
+#             strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+#         strip.show()
+#         time.sleep(wait_ms/1000.0)
+
+# def theaterChaseRainbow(strip, wait_ms=50):
+#     """Rainbow movie theater light style chaser animation."""
+#     for j in range(256):
+#         for q in range(3):
+#             for i in range(0, strip.numPixels(), 3):
+#                 strip.setPixelColor(i+q, wheel((i+j) % 255))
+#             strip.show()
+#             time.sleep(wait_ms/1000.0)
+#             for i in range(0, strip.numPixels(), 3):
+#                 strip.setPixelColor(i+q, 0)
+
+# Main program logic follows:
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    #demo_input [object (0-2),distance (10-25),orientation (0-8)
+    demo_input = [[0,20,3],[1,10,7],[2,25,0]]
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    print ('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
+    try:
+
+        while True:
+            print('Eyes On Demo')
+            for pattern in demo_input:
+                buzz(strip,pattern)
+            # print ('Color wipe animations.')
+            # colorWipe(strip, Color(255, 0, 0))  # Red wipe
+            # colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+            # colorWipe(strip, Color(0, 0, 255))  # Green wipe
+            # print ('Theater chase animations.')
+            # theaterChase(strip, Color(127, 127, 127))  # White theater chase
+            # theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+            # theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+            # print ('Rainbow animations.')
+            # rainbow(strip)
+            # rainbowCycle(strip)
+            # theaterChaseRainbow(strip)
+
+    except KeyboardInterrupt:
+        if args.clear:
+            colorWipe(strip, Color(0,0,0), 10)
