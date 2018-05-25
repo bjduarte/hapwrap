dc38514c40f099f30030ae22117e4482e4e6c6ad
RedEyeDragon <bjduarte@asu.edu>
2018-05-24 20:23:29 -0700

    trouble shooting radio button errors

--- python/examples/haptic_vest_GUI.py
+++ python/examples/haptic_vest_GUI.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python3
+
 import wx
 
-class Example(wx.Frame):
+class Haptic_Vest(wx.Frame):
 
    def __init__(self, parent, title):
-      super(Example, self).__init__(parent, title = title, size = (420, 270))
+      super(Haptic_Vest, self).__init__(parent, title = title, size = (420, 270))
 
       self.InitUI()
       self.Centre()
@@ -22,23 +24,41 @@ class Example(wx.Frame):
 
       self.elevationBox = wx.RadioBox(panel, label = 'ElevationRadioBox', choices = elevationList,
          majorDimension = 1, style = wx.RA_SPECIFY_COLS)
-      #self.elevationBox.Bind(wx.EVT_RADIOBOX, self.onElevationRadioBox)
+      self.elevationBox.Bind(wx.EVT_RADIOBOX, self.onElevationRadioBox)
 
 
       self.distanceBox = wx.RadioBox(panel, label = 'DistanceRadioBox', choices = distanceList,
          majorDimension = 1, style = wx.RA_SPECIFY_COLS)
-      #self.distanceBox.Bind(wx.EVT_RADIOBOX, self.onDistanceRadioBox)
+      self.distanceBox.Bind(wx.EVT_RADIOBOX, self.onDistanceRadioBox)
+
 
       self.directionBox = wx.RadioBox(panel, label = 'directionRadioBox', choices = directionList,
          majorDimension = 1, style = wx.RA_SPECIFY_COLS)
-      #self.directionBox.Bind(wx.EVT_RADIOBOX, self.onDirectionRadioBox)
+      self.directionBox.Bind(wx.EVT_RADIOBOX, self.onDirectionRadioBox)
+
 
       fgs.AddMany([(self.elevationBox, 1, wx.EXPAND), (self.distanceBox, 1, wx.EXPAND), (self.directionBox, 1, wx.EXPAND)])
       fgs.AddGrowableRow(1, 1)
       fgs.AddGrowableCol(1, 1)
       hbox.Add(fgs, proportion = 2, flag = wx.ALL|wx.EXPAND, border = 15)
       panel.SetSizer(hbox)
+      
+      def onElevationRadioBox(self, e):
+        print(self.elevationBox.GetStringSelection(),' is at index ',self.elevationBox.GetSelection())
 
-app = wx.App()
-Example(None, title = 'FlexiGrid Demo')
-app.MainLoop()
+
+    # def onDistanceRadioBox(self,e):
+    #   print(self.distanceBox.GetStringSelection(),' is at index ',self.distanceBox.GetSelection())
+    #
+    #
+    # def onDirectionRadioBox(self, e):
+    #   print(self.directionRadioBox.GetStringSelection(), 'is at index ', self.directionRadioBox.GetSelection())
+
+
+# Run the program
+if __name__ == "__main__":
+    app = wx.App(False)
+    frame = Haptic_Vest(None, 'Elevation and Distance')
+    frame.Show()
+
+    app.MainLoop()
