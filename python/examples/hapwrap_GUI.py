bdaf83ed1f912179d1bc94c0dee55eb0d81ee5c4
sgill3 <sanagill3@gmail.com>
2018-06-28 11:12:55 -0700

    added static code to hapwrap_GUI

--- python/examples/hapwrap_GUI.py
+++ python/examples/hapwrap_GUI.py
@@ -10,6 +10,23 @@ import time
 from complete_hapwrap_handler import *
 from dynamic_pattern_list_builder import *
 
+# LED strip configuration:
+LED_COUNT = 24 # Number of LED pixels.
+LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN = 10 # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10 # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+pulse_on = Color(255, 255, 255)
+pulse_off = Color(0, 0, 0)
+
+hapwrap = Complete_hapwrap_handler()
+heartbeat_pulse = 3
+heartbeat_gap = 0.07 # gap between beats
+
 Root=Tk()
 RTitle=Root.title("HapWrap")
 RWidth=Root.winfo_screenwidth()
@@ -99,22 +116,10 @@ for i in elevation:
       patternDict['pattern list'] = patternList
       num += 1
 
-# LED strip configuration:
-LED_COUNT = 24 # Number of LED pixels.
-LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN = 10 # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10 # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-
-pulse_on = Color(255, 255, 255)
-pulse_off = Color(0, 0, 0)
-
-hapwrap = Complete_hapwrap_handler()
-heartbeat_pulse = 3
-heartbeat_gap = 0.07 # gap between beats
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# Initialize the library (must be called once before other functions).
+strip.begin()
 
 # Dictionary containing object positions
 patterns = {
