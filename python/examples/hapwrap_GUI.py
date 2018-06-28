81b80543099df2722ce537d97e07fe8126bf94dd
sgill3 <sanagill3@gmail.com>
2018-06-28 13:21:24 -0700

    put back ttk. in hapwrap_GUI

--- python/examples/hapwrap_GUI.py
+++ python/examples/hapwrap_GUI.py
@@ -432,35 +432,35 @@ dynamicNextButton = ttk.Button(dynamicPage, text='Next Dynamic Pattern', command
 dynamicNextButton.place(x=RWidth - RWidth/7, y=RHeight - 220, anchor=tk.CENTER)
 
 #create dynamic Save button
-dynamicSaveButton = Root.Button(dynamicPage, text = "Save", command=dynamicSaveClick, width = 15)
+dynamicSaveButton = ttk.Button(dynamicPage, text = "Save", command=dynamicSaveClick, width = 15)
 dynamicSaveButton.place(x=RWidth - 2*RWidth/7, y=RHeight - 220, anchor=tk.CENTER)
 
 #create static Save button
-staticSaveButton = Root.Button(staticPage, text = "Save", command=staticSaveClick, width = 15)
+staticSaveButton = ttk.Button(staticPage, text = "Save", command=staticSaveClick, width = 15)
 staticSaveButton.place(x=RWidth - 2*RWidth/7, y=RHeight - 220, anchor=tk.CENTER)
 
 #create dynamic restore button
-restoreButton = Root.Button(dynamicPage, text = "Restore", command=restoreClick, width = 15)
+restoreButton = ttk.Button(dynamicPage, text = "Restore", command=restoreClick, width = 15)
 restoreButton.place(x=RWidth - 5*RWidth/7, y=RHeight - 220, anchor=tk.CENTER)
 
 #create static restore button
-restoreButton = Root.Button(staticPage, text = "Restore", command=restoreClick, width = 15)
+restoreButton = ttk.Button(staticPage, text = "Restore", command=restoreClick, width = 15)
 restoreButton.place(x=RWidth - 5*RWidth/7, y=RHeight - 220, anchor=tk.CENTER)
 
 #create dynamic repeat button
-repeatButton = Root.Button(dynamicPage, text = "Repeat", command=repeatClick, width = 15)
+repeatButton = ttk.Button(dynamicPage, text = "Repeat", command=repeatClick, width = 15)
 repeatButton.place(x=RWidth - 6*RWidth/7, y=RHeight - 220, anchor=tk.CENTER)
 
 #create static repeat button
-repeatButton = Root.Button(staticPage, text = "Repeat", command=repeatClick, width = 15)
+repeatButton = ttk.Button(staticPage, text = "Repeat", command=repeatClick, width = 15)
 repeatButton.place(x=RWidth - 6*RWidth/7, y=RHeight - 220, anchor=tk.CENTER)
 
  #create static status text
-statusMessage = Root.Label(staticPage, text="Status: UNSAVED")
+statusMessage = ttk.Label(staticPage, text="Status: UNSAVED")
 statusMessage.place(x=RWidth - 2*RWidth/7, y=RHeight-190, height=1, width=15, anchor=tk.CENTER)
 
  #create dynamic status text
-statusMessage = Root.Label(dynamicPage, text="Status: UNSAVED")
+statusMessage = ttk.Label(dynamicPage, text="Status: UNSAVED")
 statusMessage.place(x=RWidth - 2*RWidth/7, y=RHeight-190, height=1, width=15, anchor=tk.CENTER)
 
 # Create NeoPixel object with appropriate configuration.
