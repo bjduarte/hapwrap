6aa30a685bd7c52a4ae9c804d43b6b3ac446cee9
sgill3 <sanagill3@gmail.com>
2018-11-11 16:17:14 -0700

    fixed repeated zeros

--- python/examples/hapwrap_GUI.py
+++ python/examples/hapwrap_GUI.py
@@ -659,7 +659,7 @@ def restoreStaticClick():
         for i in fin['static counter']:
             staticCounter.append(i)
         for i in fin['Static Repeat Counter']:
-            staticRepeatCounter.append(i)
+            staticRepeatCounter.append(i-1)
         staticPatternNum = fin['static counter'][-1] - 1
 
     except:
@@ -691,9 +691,9 @@ def restoreDynamicClick():
         for i in fin['dynamic counter']:
             dynamicCounter.append(i)
         for i in fin['Static Repeat Counter']:
-            staticRepeatCounter.append(i)
+            staticRepeatCounter.append(i-1)
         for i in fin['Dyanmic Repeat Counter']:
-            dynamicRepeatCounter.append(i)
+            dynamicRepeatCounter.append(i-1)
         dynamicPatternNum = fin['dynamic counter'][-1] - 1
     except:
         try:
@@ -708,7 +708,7 @@ def restoreDynamicClick():
             for i in fin['static counter']:
                 staticCounter.append(i)
             for i in fin['Static Repeat Counter']:
-                staticRepeatCounter.append(i)
+                staticRepeatCounter.append(i-1)
             dynamicPatternNum = fin['dynamic counter'][-1] - 1
         except:
             try:
@@ -719,7 +719,7 @@ def restoreDynamicClick():
                 for i in fin['dynamic counter']:
                     dynamicCounter.append(i)
                 for i in fin['Dyanmic Repeat Counter']:
-                    dynamicRepeatCounter.append(i)
+                    dynamicRepeatCounter.append(i-1)
                 dynamicPatternNum = fin['dynamic counter'][-1] - 1
             except:
                 print("nothing to restore")
