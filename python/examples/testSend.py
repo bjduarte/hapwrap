cdf53b0740812f05b9c05f55412a83371ef18aa9
RedEyeDragon <bjduarte@asu.edu>
2019-02-06 19:53:10 -0700

    debug sending

--- python/examples/testSend.py
+++ python/examples/testSend.py
@@ -11,7 +11,7 @@ API_ENDPOINT = "http://172.20.10.13:8080/postjson"
 
 #data = "{'name' : ['Bryan', 'Sana', 'Bijan', 'Troy', 'Meredith', 'Arash', 'Ramesh', 'Ramin', 'Hemanth'],'gender' : ['male', 'female', 'male', 'male', 'female', 'male', 'female', 'male', 'male'],'age' : [0, 0, 24, 0, 25, 0, 24, 0, 0]}"
 
-toSend =  {[[1, 3, 2], [1, 2, 1], [1, 1, 1], [1, 0, 0], [1, 1, 7], [1, 2, 7], [1, 3, 6]]}
+toSend = [[1, 3, 2], [1, 2, 1], [1, 1, 1], [1, 0, 0], [1, 1, 7], [1, 2, 7], [1, 3, 6]]
 
 data = json.dumps(toSend)
 
