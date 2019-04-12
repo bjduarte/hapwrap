b9a7e3d5ab205305f1b395d57d3ed3c7f7636d63
AbhikChowdhury6 <abhik.ch6@gmail.com>
2019-04-12 20:52:56 +0000

    updated timings for hapBack

--- AbhiksPython/hapBack.py
+++ AbhiksPython/hapBack.py
@@ -1,6 +1,8 @@
 from hapServer import *
 import time
 
+
+#diret
 def a1(targetRow, hapserver):
     hframes = hapFrames()
 
@@ -9,6 +11,8 @@ def a1(targetRow, hapserver):
 
     hapserver.send(hframes.frames)
 
+
+#back and forth
 def a2(targetRow, hapServer):
     hframes = hapFrames()
 
@@ -28,11 +32,16 @@ def a2(targetRow, hapServer):
     hapServer.send(hframes.frames)
 
 
+#ladder
 def r1(targetRow, hapServer):
     hframes = hapFrames()
+    waitTime = 1000
 
     onTime = 200
     offTime = 100
+
+    hframes.addBWFrame(waitTime,[],10)
+
 
     for i in range(targetRow):
         hframes.addBWFrame(onTime,[i, 9-i],10)
@@ -41,24 +50,28 @@ def r1(targetRow, hapServer):
     hapServer.send(hframes.frames)
 
 
-
+#zig zag
 def r2(targetRow, hapServer):
     hframes = hapFrames()
+    waitTime = 1000
 
     onTime = 200
     inBetweenTime = 100
     offTime = 200
 
+    hframes.addBWFrame(waitTime,[],10)
+
+
     for i in range(targetRow):
         hframes.addBWFrame(onTime,[i],10)
-        hframes.addBWFrame(inBetweenTime,[],10)
+#        hframes.addBWFrame(inBetweenTime,[],10)
         hframes.addBWFrame(onTime,[9-i],10)
-        hframes.addBWFrame(offTime,[],10)
+#        hframes.addBWFrame(offTime,[],10)
     
-    for i in range(targetRow,-1,-1):
-        hframes.addBWFrame(onTime,[9-i],10)
-        hframes.addBWFrame(inBetweenTime,[],10)
-        hframes.addBWFrame(onTime,[i],10)
-        hframes.addBWFrame(offTime,[],10)
+    # for i in range(targetRow-1,-1,-1):
+    #     hframes.addBWFrame(onTime,[9-i],10)
+    #     hframes.addBWFrame(inBetweenTime,[],10)
+    #     hframes.addBWFrame(onTime,[i],10)
+    #     hframes.addBWFrame(offTime,[],10)
 
     hapServer.send(hframes.frames)
--- AbhiksPython/hapBackTestUI.py
+++ AbhiksPython/hapBackTestUI.py
@@ -1,13 +1,20 @@
 from hapServer import hapserver
 import hapBack as hb
-
+import time
 
 hs = hapserver()
 
-hb.a1(3,hs)
+# hb.a1(1,hs)
+# time.sleep(4)
+
 
 hb.a2(4,hs)
+time.sleep(4)
 
-hb.r1(4,hs)
 
-hb.r2(4,hs)
+# hb.r1(4,hs)
+# time.sleep(4)
+
+
+# hb.r2(5,hs)
+# time.sleep(4)
