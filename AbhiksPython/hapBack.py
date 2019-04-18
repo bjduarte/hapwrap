77aee026f4d1e6162dbf5c6e7d67323c50f9ae66
AbhikChowdhury6 <abhik.ch6@gmail.com>
2019-04-18 22:05:34 +0000

    updated numbering order on hapback

--- AbhiksPython/hapBack.py
+++ AbhiksPython/hapBack.py
@@ -1,13 +1,14 @@
-from AbhiksPython.hapServer import *
+from hapServer import *
 import time
 
 
 #diret
 def a1(targetRow, hapserver):
     hframes = hapFrames()
+    stripTargetRow = 5-targetRow
 
     onTime = 500
-    hframes.addBWFrame(onTime,[targetRow, 9-targetRow],10)
+    hframes.addBWFrame(onTime,[stripTargetRow, 9-stripTargetRow],10)
 
     hapserver.send(hframes.frames)
 
@@ -15,18 +16,19 @@ def a1(targetRow, hapserver):
 #back and forth
 def a2(targetRow, hapServer):
     hframes = hapFrames()
+    stripTargetRow = 5-targetRow
 
     onTime = 200
     inBetweenTime = 100
     offTime = 200
 
-    hframes.addBWFrame(onTime,[targetRow],10)
+    hframes.addBWFrame(onTime,[stripTargetRow],10)
 #    hframes.addBWFrame(inBetweenTime,[],10)
-    hframes.addBWFrame(onTime,[9-targetRow],10)
+    hframes.addBWFrame(onTime,[9-stripTargetRow],10)
 #    hframes.addBWFrame(offTime,[],10)
-    hframes.addBWFrame(onTime,[targetRow],10)
+    hframes.addBWFrame(onTime,[stripTargetRow],10)
 #    hframes.addBWFrame(inBetweenTime,[],10)
-    hframes.addBWFrame(onTime,[9-targetRow],10)
+    hframes.addBWFrame(onTime,[9-stripTargetRow],10)
 
 
     hapServer.send(hframes.frames)
@@ -35,6 +37,7 @@ def a2(targetRow, hapServer):
 #ladder
 def r1(targetRow, hapServer):
     hframes = hapFrames()
+    stripTargetRow = 6-targetRow
     waitTime = 1000
 
     onTime = 150
@@ -43,7 +46,7 @@ def r1(targetRow, hapServer):
     hframes.addBWFrame(waitTime,[],10)
 
 
-    for i in range(targetRow):
+    for i in range(5-stripTargetRow):
         hframes.addBWFrame(onTime,[i, 9-i],10)
 #        hframes.addBWFrame(offTime,[],10)
 
@@ -53,6 +56,8 @@ def r1(targetRow, hapServer):
 #zig zag
 def r2(targetRow, hapServer):
     hframes = hapFrames()
+    stripTargetRow = 6-targetRow
+
     waitTime = 1000
 
     onTime = 200
@@ -62,13 +67,13 @@ def r2(targetRow, hapServer):
     hframes.addBWFrame(waitTime,[],10)
 
 
-    for i in range(targetRow):
+    for i in range(stripTargetRow):
         hframes.addBWFrame(onTime,[i],10)
 #        hframes.addBWFrame(inBetweenTime,[],10)
         hframes.addBWFrame(onTime,[9-i],10)
 #        hframes.addBWFrame(offTime,[],10)
     
-    # for i in range(targetRow-1,-1,-1):
+    # for i in range(stripTargetRow-1,-1,-1):
     #     hframes.addBWFrame(onTime,[9-i],10)
     #     hframes.addBWFrame(inBetweenTime,[],10)
     #     hframes.addBWFrame(onTime,[i],10)
--- AbhiksPython/hapBackTestUI.py
+++ AbhiksPython/hapBackTestUI.py
@@ -1,5 +1,5 @@
-from AbhiksPython.hapServer import hapserver
-import AbhiksPython.hapBack as hb
+from hapServer import hapserver
+import hapBack as hb
 import time
 import sys
 
@@ -7,17 +7,17 @@ hs = hapserver()
 
 print(sys.argv[1])
 if sys.argv[1] == "1":
-    hb.a1(5-int(sys.argv[2]),hs)
+    hb.a1(int(sys.argv[2]),hs)
     time.sleep(1)
 
 if sys.argv[1] == "2":
-    hb.a2(5-int(sys.argv[2]),hs)
+    hb.a2(int(sys.argv[2]),hs)
     time.sleep(1)
 
 if sys.argv[1] == "3":
-    hb.r1(6-int(sys.argv[2]),hs)
+    hb.r1(int(sys.argv[2]),hs)
     time.sleep(1)
 
 if sys.argv[1] == "4":
-    hb.r2(6-int(sys.argv[2]),hs)
+    hb.r2(int(sys.argv[2]),hs)
     time.sleep(1)
