0bd16d4bff74d406934e6f64be96611ae6be6616
Abhik Chowdhury <abhik.ch6@gmail.com>
2020-01-06 16:53:35 -0700

    added other half of client code

--- computerVision/client.py
+++ computerVision/client.py
@@ -15,3 +15,15 @@ args = vars(ap.parse_args())
 # server
 sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
 	args["server_ip"]))
+
+# get the host name, initialize the video stream, and allow the
+# camera sensor to warmup
+rpiName = socket.gethostname()
+vs = VideoStream(usePiCamera=True).start()
+#vs = VideoStream(src=0).start()
+time.sleep(2.0)
+ 
+while True:
+	# read the frame from the camera and send it to the server
+	frame = vs.read()
+	sender.send_image(rpiName, frame)
